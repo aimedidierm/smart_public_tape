9c2615ff2dee7120b8fe169461e84a337109b6a6
aimedidierm <aimedidiermugisha@gmail.com>
2022-07-17 18:32:23 +0200

    Authentication and authorisation

---
+++ login.php
@@ -0,0 +1,32 @@
+<?php 
+session_start();
+require 'php-includes/connect.php';
+extract($_POST);
+$password=md5($password);
+
+$query = "SELECT * FROM seller WHERE email= ? AND password = ? limit 1";
+$stmt = $db->prepare($query);
+$stmt->execute(array($email, $password));
+$rows = $stmt->fetchAll();
+if ($stmt->rowCount()>0) {
+	$_SESSION['email'] = $email;
+	$_SESSION['id'] = session_id();
+	$_SESSION['login_type'] = "seller";
+
+	echo "<script>alert('You Are Logged In');window.location.assign('seller/dashboard.php')</script>";
+}
+
+
+$query = "SELECT * FROM admin WHERE email= ? AND password = ? limit 1";
+$stmt = $db->prepare($query);
+$stmt->execute(array($email, $password));
+$rows = $stmt->fetchAll();
+if ($stmt->rowCount()>0) {
+	$_SESSION['email'] = $email;
+	$_SESSION['id'] = session_id();
+	$_SESSION['login_type'] = "admin";
+
+	echo "<script>alert('You Are Logged In');window.location.assign('admin/dashboard.php')</script>";
+}else{
+	echo "<script>alert('Your ID or Password is Wrong');window.location.assign('index.php')</script>";
+}
